c2f93cf3a791a5152dc1efaaae8bfd9735c48aff
JM00001 <liser1@naver.com>
2023-07-28 09:07:41 +0900

    .
    .

--- BOARD-계층형_EML_DQL.sql
+++
@@ -1,8 +0,0 @@
---DROP TABLE "BOARD_READ_RECORD";
---DROP TABLE "BOARD";
---DROP TABLE "MEMBER";
-desc member;
-insert into MEMBER values ('kh1', '11','kh1name','kh1@a.co.kr')
-;
-insert into MEMBER values ('admin','admin','adminname','admin@a.co.kr')
-;
